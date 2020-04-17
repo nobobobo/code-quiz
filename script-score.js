8f9fae4c76ce387b3ec2cae720f54074a9144153
nobobobo <ianhsu1221@gmail.com>
2020-04-17 15:53:28 -0700

    added render function to display scores

--- script-score.js
+++ script-score.js
@@ -1,5 +1,6 @@
-let scores = JSON.parse(localStorage.getItem("scores"));
+// get scores from local storage and sort by comparing each object's score
 
+let scores = JSON.parse(localStorage.getItem("scores"));
 
 function compare(scoreA, scoreB) {
     let a = scoreA.score;
@@ -12,3 +13,36 @@ function compare(scoreA, scoreB) {
 }
 
 scores.sort(compare);
+
+
+// select DOM elements
+
+let tableBody = document.querySelector("#tableBody");
+
+// call renderScores to display scores loaded from local storage.
+renderScores();
+
+
+
+
+
+
+
+
+
+// function: renderScores()
+function renderScores() {
+    tableBody.innerHTML = "";
+    for (let i = 0; i < scores.length; i++) {
+        let name = scores[i].name;
+        let score = scores[i].score;
+        let newTr = document.createElement("tr");
+        newTr.innerHTML = "<th scope \"row\">" + (i + 1) + "</th><td>" + name + "</td><td>" + score + "</td></tr>";
+        tableBody.appendChild(newTr);
+    }
+}
+
+function clearScores(){
+    localStorage.clear();
+    renderScores();
+}
