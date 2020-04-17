f1816bbbcd0774e2bfa38d2fd2bfcc0a8d115e42
nobobobo <ianhsu1221@gmail.com>
2020-04-16 19:09:42 -0700

    added quiz questions list, startQuiz function, render function

--- script.js
+++ script.js
@@ -1,40 +1,76 @@
-// make a list of objects representing the quiz data
-// remember that when we want to organize data in JS we typically use objects
+// declare div elements by selecting ids
+let startBtn = document.querySelector("#startBtn");
+let welcomePage = document.querySelector("#welcomePage");
+let question = document.querySelector("#question");
+let options = document.querySelector("#options")
+
+// define quiz questions
 let quizQuestions = [
     {
-      question: "is javascript cool?",
-      choices: ["yes", "no"],
-      answer: "yes",
+        question: "What JS method is used to print a string to console?",
+        choices: ["print()", "System.out.println()", "console.log()", "printf()"],
+        answer: "console.log()",
     },
     {
-      question: "is HTML cool?",
-      choices: ["yes", "no"],
-      answer: "yes"
+        question: "is HTML cool?",
+        choices: ["yes", "no"],
+        answer: "yes"
     },
     {
-      question: "is CSS cool?",
-      choices: ["yes", "no"],
-      answer: "yes"
+        question: "is CSS cool?",
+        choices: ["yes", "no"],
+        answer: "yes"
     },
     {
-      question: "do it ever be like it do?",
-      choices: ["do it yes be", "no it dont do like it be"],
-      answer: "do it yes be"
+        question: "do it ever be like it do?",
+        choices: ["do it yes be", "no it dont do like it be"],
+        answer: "do it yes be"
     },
-  ];
-  // keep track of the question we are currently on
-  let currentQuestion = 0;
-  function render() {
-    let questionData = quizQuestions[currentQuestion];
-    $("#question").text(questionData.question);
-    $("#options").empty();
-    for (let choice of questionData.choices) {
-      $("#options").append($("<li>").text(choice))
+];
+
+
+// keep track of the question we are currently on
+let currentQuestionId = 0;
+
+
+// startQuiz function will render out questions and start a timer
+function startQuiz() {
+    welcomePage.style.display = 'none';
+    render();
+
+}
+
+// add a event listener to "startBtn" to listen a click event. 
+startBtn.addEventListener("click", startQuiz);
+
+
+// display quiz question and options 
+function render() {
+    let questionData = quizQuestions[currentQuestionId];
+    question.textContent = questionData.question;
+    options.innerHTML = '';
+
+    currentQuestionId += 1;
+
+    for (let i = 0; i < questionData.choices.length; i++) {
+        let choice = questionData.choices[i];
+        let newElm = document.createElement("li");
+        newElm.innerHTML = "<button id =\"" + choice + "\" class =\"btn btn-info btn-xs\">" + choice + "</button>";
+        newElm.addEventListener("click", function(){
+            if (choice !== questionData.answer) {
+                document.getElementById("time").textContent -= 10;
+            }
+
+            if (currentQuestionId === quizQuestions.length){
+                console.log("fin");
+            } else {
+                render();
+            }
+        });
+        options.appendChild(newElm);
     }
-    currentQuestion += 1;
-    if (currentQuestion === questionData.length) {
-        currentQuestion = 0;
-    }
-  }
-  $("#submit").click(render)
-render()
+
+    
+}
+
+
