375a97a4113744a0fc44b6c0867e73facf82ec09
nobobobo <ianhsu1221@gmail.com>
2020-04-17 16:07:26 -0700

    added feature to clear all score from localstorage and table

--- script-score.js
+++ script-score.js
@@ -1,5 +1,11 @@
-// get scores from local storage and sort by comparing each object's score
+// select DOM elements
 
+let tableBody = document.querySelector("#tableBody");
+let goBack = document.querySelector("#goBack");
+let clearScore = document.querySelector("#clearScore");
+
+
+// get scores from local storage and sort by comparing each object's score
 let scores = JSON.parse(localStorage.getItem("scores"));
 
 function compare(scoreA, scoreB) {
@@ -12,37 +18,39 @@ function compare(scoreA, scoreB) {
     return 0;
 }
 
-scores.sort(compare);
+// sort obejct array, and call renderScores to display scores loaded from local storage.
+
+if (scores !== null) {
+    scores.sort(compare);
+    renderScores();
+}
 
 
-// select DOM elements
-
-let tableBody = document.querySelector("#tableBody");
-
-// call renderScores to display scores loaded from local storage.
-renderScores();
+// event listener for clicking on goBack button
+goBack.addEventListener("click", function () {
+    location.href = "index.html";
+});
 
 
-
-
-
-
-
+// event listener for clicking on Clear All button
+clearScore.addEventListener("click", clearScores);
 
 
 // function: renderScores()
 function renderScores() {
-    tableBody.innerHTML = "";
-    for (let i = 0; i < scores.length; i++) {
-        let name = scores[i].name;
-        let score = scores[i].score;
-        let newTr = document.createElement("tr");
-        newTr.innerHTML = "<th scope \"row\">" + (i + 1) + "</th><td>" + name + "</td><td>" + score + "</td></tr>";
-        tableBody.appendChild(newTr);
+    if (scores !== null) {
+        tableBody.innerHTML = "";
+        for (let i = 0; i < scores.length; i++) {
+            let name = scores[i].name;
+            let score = scores[i].score;
+            let newTr = document.createElement("tr");
+            newTr.innerHTML = "<th scope \"row\">" + (i + 1) + "</th><td>" + name + "</td><td>" + score + "</td></tr>";
+            tableBody.appendChild(newTr);
+        }
     }
 }
 
-function clearScores(){
+function clearScores() {
     localStorage.clear();
-    renderScores();
+    tableBody.innerHTML = "";
 }
